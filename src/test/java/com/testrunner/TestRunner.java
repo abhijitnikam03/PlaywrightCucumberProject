74d554e63afcfb10a5a66c191b342adc818d7f91
abhij <abhij@Abhijit.lan>
2025-01-15 12:23:02 +0530

    test runner

--- src/test/java/com/testrunner/TestRunner.java
+++ src/test/java/com/testrunner/TestRunner.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 //@RunWith(Cucumber.class)
-@CucumberOptions(features = { "src/test/resource/features" },
+@CucumberOptions(features = { "src/test/resource/features/practice.feature" },
 		// tags = "@Positive_UnFreezeNSDLMaker",
 		glue = { "apphook", "com/stepdef" }, dryRun = false, plugin = { "pretty",
 				"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:" }, monochrome = true)
